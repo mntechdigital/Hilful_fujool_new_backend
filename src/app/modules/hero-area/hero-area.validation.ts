a2be874ed868622a9f8f8a9f0e8fbfce26406df8
reshad21 <miakala7@gmail.com>
2025-12-28 15:58:20 +0600

    Initial commit to new repository

---
+++ src/app/modules/hero-area/hero-area.validation.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+const createHeroSectionValidation = z.object({
+  body: z.object({
+    subtitle: z.string({ required_error: 'Subtitle is required' }),
+    title: z.string({ required_error: 'Title is required' }),
+    youtubeUrl: z.string({ required_error: 'YouTube URL is required' }),
+    images: z.string().optional(),
+  }),
+});
+
+const updateHeroSectionValidation = z.object({
+  body: z.object({
+    subtitle: z.string().optional(),
+    title: z.string().optional(),
+    youtubeUrl: z.string().optional(),
+    images: z.string().optional(),
+  }),
+});
+
+export const HeroAreaValidation = {
+  createHeroSectionValidation,
+  updateHeroSectionValidation,
+};
